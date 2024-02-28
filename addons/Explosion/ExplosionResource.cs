68a7b14b02eab659aa979d6b9c4fd473eb61d848
Andre <nipsky@googlemail.com>
2024-02-28 01:56:31 +0100

    Cleanup

--- addons/Explosion/ExplosionResource.cs
+++ addons/Explosion/ExplosionResource.cs
@@ -1,8 +1,11 @@
+using System.Reflection;
 using Godot;
 
 public partial class ExplosionResource : Resource
 {
+
     [Export]
+    // Leave duration at 0.0f to use the duration of the animation or sound
     public float MaxDuration { get; set; } = 0.0f;
 
     [Export]
